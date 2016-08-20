c2678d954432a8c02071ddc144b983d06aff453b
Kolten <koltensturgill92@gmail.com>
2016-08-20 11:37:02 -0500

    Commit

---
+++ att-hack/MapViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  att-hack
+//
+//  Created by Kolten Sturgill on 8/20/16.
+//  Copyright © 2016 Kolten. All rights reserved.
+//
+
+import UIKit
+import Mapbox
+
+class MapViewController: UIViewController, MGLMapViewDelegate {
+
+    var walk: Walk?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
