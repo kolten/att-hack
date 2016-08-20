bd8c773383675d45068a569a871bfa6885600024
Cameron Moreau <cameron.cm6@gmail.com>
2016-08-20 04:02:13 -0500

    Added users

---
+++ att-hack/FamilyCollectionViewController.swift
@@ -0,0 +1,76 @@
+//
+//  FamilyCollectionViewController.swift
+//  att-hack
+//
+//  Created by Cameron Moreau on 8/20/16.
+//  Copyright © 2016 Kolten. All rights reserved.
+//
+
+import UIKit
+
+// TODO: Make cell clickable with action sheet for call or text
+class FamilyCollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 4
+    }
+
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath) as! UserCollectionViewCell
+    
+        let u = User(name: "Sarah", phone: "")
+        
+        cell.userName.text = u.name
+    
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(collectionView: UICollectionView, shouldHighlightItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(collectionView: UICollectionView, shouldSelectItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(collectionView: UICollectionView, shouldShowMenuForItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(collectionView: UICollectionView, canPerformAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) -> Bool {
+        return false
+    }
+
+    override func collectionView(collectionView: UICollectionView, performAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) {
+    
+    }
+    */
+
+}
---
+++ att-hack/UserCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  UserCollectionViewCell.swift
+//  att-hack
+//
+//  Created by Cameron Moreau on 8/20/16.
+//  Copyright © 2016 Kolten. All rights reserved.
+//
+
+import UIKit
+
+class UserCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var userImage: UIImageView!
+    @IBOutlet weak var userName: UILabel!
+}
